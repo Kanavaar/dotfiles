d1ec883fdce69b7b474d017555ce51e40cd79134
kanavaar <uju8765@gmail.com>
2022-11-27 15:19:18 +0100

    optional darkmode

---
+++ qutebrowser/darkmode.py
@@ -0,0 +1,22 @@
+
+# load your autoconfig, use this if the rest of your config is empty!
+config.load_autoconfig()
+
+c.url.start_pages = ["https://www.wttr.in/"]
+c.url.searchengines = {"DEFAULT": "https://searx.org/search?q={}"}
+
+c.fonts.tabs.selected = '8pt JetBrains Mono Nerd Font'
+c.fonts.tabs.unselected = '8pt JetBrains Mono Nerd Font'
+c.fonts.hints = '8pt JetBrains Mono Nerd Font'
+c.fonts.keyhint = '8pt JetBrains Mono Nerd Font'
+c.fonts.prompts = '8pt JetBrains Mono Nerd Font'
+c.fonts.downloads = '8pt JetBrains Mono Nerd Font'
+c.fonts.statusbar = '8pt JetBrains Mono Nerd Font'
+c.fonts.contextmenu = '8pt JetBrains Mono Nerd Font'
+c.fonts.messages.info = '8pt JetBrains Mono Nerd Font'
+c.fonts.debug_console = '8pt JetBrains Mono Nerd Font'
+c.fonts.completion.entry = '8pt JetBrains Mono Nerd Font'
+
+#config.set('colors.webpage.darkmode.enabled', True)
+c.colors.webpage.darkmode.enabled = True
+
