2d93b6b0be8734c2a6a0bb75c800aa13c81423c6
Kanavaar <uju8765@gmail.com>
2023-01-07 00:21:14 +0100

    updating script

--- qtile/scripts/autostart.sh
+++ qtile/scripts/autostart.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+killall picom
+killall dunst
+
 function run {
   if ! pgrep $1 ;
   then
@@ -10,15 +13,18 @@ function run {
 #start sxhkd to replace Qtile native key-bindings
 run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
 
+xrandr --output DP-0 --mode 1920x1080 --rate 144
 #start mpd
 [ ! -s ~/.config/mpd/pid ] && mpd &
 
 setxkbmap -option ctrl:nocaps &
 clipmenud &
-dunst &
+numlockx &
 
 #starting utility applications at boot time
 picom --vsync &
-/usr/libexec/polkit-gnome-autentication-agent-1 &
 #/usr/lib/xfce4/notifyd/xfce4-notifyd &
-~/.fehbg &
+
+# xwallpaper --maximize ~/pix/wallhaven-q29d2l_1920x1080_gruv.png
+feh -r --bg-scale --randomize ~/pix/walls/*.png
+dunst &
