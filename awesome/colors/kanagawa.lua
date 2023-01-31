718a280f2a0afb5e909afe19403eec765994e3da
kanavaar <uju8765@gmail.com>
2023-01-31 15:45:29 +0100

    adding wallpaper

---
+++ awesome/colors/kanagawa.lua
@@ -0,0 +1,18 @@
+-- Awesome Config | Kanagawa
+
+return {
+	bg = "#1f1f28",
+	fg = "#dcd7ba",
+	black = "#090618",
+	red = "#c34043",
+	green = "#76946a",
+	yellow = "#c0a36e",
+	blue = "#7e9cd8",
+	magenta = "#957fb8",
+	cyan = "#6a9589",
+	white = "#c8c093",
+	darkgrey = "#727169",
+	lightgrey = "#dcd7ba",
+	fff = "#fff",
+	zero = "#000",
+}
