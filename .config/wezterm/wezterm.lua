7bd6e05464600c31d5798e8cb7a8d073ee11397b
kanavaar <uju8765@gmail.com>
2023-05-18 01:33:54 +0200

    updating colorscheme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,13 +3,17 @@ local font= require("cfg.font")
 
 return {
   font = wezterm.font(font),
-  color_scheme = "GruvboxDark",
+  color_scheme = "Oxocarbon Dark",
   -- color_scheme = "Catppuccin Mocha",
   window_padding = {
     top = 0,
     bottom = 0,
     left = 0,
     right = 0,
+  },
+  colors = {
+    background = "#080808",
+    foreground = "#F5F5F5",
   },
   enable_tab_bar = false,
   enable_wayland = true,
