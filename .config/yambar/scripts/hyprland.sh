5787158dec4e1d417dbc705b877be9b26aaae257
kanavaar <uju8765@gmail.com>
2023-12-25 02:15:01 +0100

    add yambar for hyprland
    Signed-off-by: kanavaar <uju8765@gmail.com>

---
+++ .config/yambar/scripts/hyprland.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+while true; do
+hypractive=$(hyprctl activeworkspace -j)
+hyprwindow=$(echo "$hypractive" | grep -E '"id": ' | tr -dc '0-9')
+
+if [ $hyprwindow -gt 10 ]
+then
+    workspace=$((hyprwindow - 10))
+else
+	workspace=$hyprwindow
+fi
+
+echo "workspace|int|$workspace"
+echo ""
+
+sleep 0.5
+done 
